9398ebe2065e300876c95347dcd55b6ecd3402c6
Daviiddoo <Davidchew12345@gmail.com>
2020-12-16 16:18:00 +0800

    Added documentation, and testing env

--- server/src/connection.ts
+++ server/src/connection.ts
@@ -1,16 +1,19 @@
 import { auth, FirebaseError } from "firebase-admin";
 import { Socket } from "socket.io";
 
-export enum Event {
+// Socket connection events being listened to
+enum Event {
     CONNECTION = 'connection',
     DISCONNECT = 'disconnect',
     UPDATE = 'update',
 }
 
+// A socket with uuid as parameter
 interface UserSocket extends Socket {
     uuid?: string;
 }
 
+// A general authentication error type
 class AuthenticationError extends Error {
     constructor(message: string) {
         super(message);
@@ -18,45 +21,52 @@ class AuthenticationError extends Error {
     }
 }
 
-/* 
-Joins authenticated users into their respective rooms,
-listens for "update" events and pushes data to the room
-*/
-export function connect(socket: UserSocket): void {
+
+// Facilitates Socket client connection to the Server
+function connect(socket: UserSocket): void {
     const uuid = socket.uuid;
     console.log(`${uuid} has connected`);
     if (uuid) {
+        // Joins user to its room according to uuid
         socket.join(uuid);
-
+        // Listens to UPDATE event
         socket.on(Event.UPDATE, (data: ArrayBuffer) => {
+            // Broadcasts Protobuf files to all sockets in the room
             socket.broadcast.to(uuid).emit(Event.UPDATE, data);
             console.log(`${uuid} group was updated`);
         });
     }
+    // Listens to DISCONNECT event
     socket.on(Event.DISCONNECT, () => {
         console.log(`${uuid} has disconnected`);
     });
 }
 
-/* 
-Autheticates user via its token
-*/
-export function authenticate(socket: UserSocket, next: (err?: Error) => void): void {
+
+// Autheticates client via its token
+function authenticate(socket: UserSocket, next: (err?: Error) => void): void {
+    // Token provided by client
     const token = socket.handshake.query['token'];
     if (token) {
+        // Verifies authenticity of the ID token with firebase-auth 
         auth().verifyIdToken(token)
             .then((decodedToken) => {
+                // Populate uuid parameter in socket
                 socket.uuid = decodedToken.uid;
                 console.log(`Succesfully verified token for ${socket.uuid}`);
                 next();
             }).catch((e: FirebaseError) => {
+                // Throws error if verification has failed
                 const error = new AuthenticationError(`${socket.uuid}'s token could not be verified: ${e}`);
                 console.error(error.message);
                 next(error);
             });
     } else {
+        // Throws error if Token was null or empty
         const error = new AuthenticationError('Token is null or empty');
         console.error(error.message);
         next(error);
     }
 }
+
+export { authenticate, connect, Event }
--- server/src/server.ts
+++ server/src/server.ts
@@ -12,9 +12,9 @@ const io = new Server(http);
 initializeApp({
     credential: credential.cert({
         // Private key is sanitized for Heroku deployment by replacing \\n characters with \n
-        privateKey: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
-        projectId: process.env.PROJECT_ID,
-        clientEmail: process.env.CLIENT_EMAIL,
+        privateKey: "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDGjL8AoR3njMrs\ncnOTKKYuRqNUITPDEGNhCUz+1cHS7Nteh47QJn45goBhRSBp6KDRMThPGcca6Ndy\nllWIc+1L2HQJiUYv6zJdgIoplP8DJxSD0Pmg282lDgajdkL4+glXGiscmlDhe4X+\n3tz50WFEI0j3/HBbT+4FYT/pVD66uhAXhxPKfBWEPDBqJFIgq03T082qvV6SqKjM\nycMZBpSwrpemjbTRrtFrcqWFF3KYpxxF4FPWOcuGjhWFZovlenSJCle+RwEJlGwo\nI0vukbLg+Y7+maYIRnyQcSSEh7gsXwdbhjeKnDSsSyOYrZ7yVjlodPzKbeqPJw8o\n07mDja+vAgMBAAECggEAAOqXTc3WTjodCRW/HlKWxSUcpmfrVizr5Jn6MgjMwlOM\nHpHDaxDaak/eQKoCW6KShPpkrWG025u+mx+KOHgyPxbRG56EvhB3GXDWtAv922sV\n+XK/Co8IU8dEwV63A8ZdzqTQDqn7TrxxRqKYQDcSrP3yfluj5V7DKOr3SAxKemP1\npnDbIZEFPpsZGPjmMOH+HbcnU+XiW8X+Vs9poM3RhMABtCO0z2By3Zlv7njulgXW\n0KsXCRbmiKdbMNOqDoulcJfx2Ax81jTAmE0/31CFmmMce7hkZuUVvl2D0FGRnFLx\n7rE4a0VkrRZVov7FqZ0qS+h10ZFvUhiGU7FqiTRb0QKBgQDo+Go/8D4Ngnj3hdLm\nhcL2umA/EbvszDnSQ/6Y1UVzyAlphBLaXTd9dDtIBUf+iNF79WmYGlsIpK2Kw5Qa\ngWL32OxnY/lVORiP13eAPJwBSskQlTn1Mc5OKGoZJpRi7bQiIFQ1voVX8G3xvvoc\ndxsPGKKXZZ2kXVgh3AT+B/M20QKBgQDaLUdEzDbbLGikarTZFqf0CoU9WPhUboQN\ns9pi8yK9UUzlGC7PpHp3AVgTWKV83hJPZ8aCNCDKtAG5jZcZ9pEXmG4UpJavQLwR\n2ZjjNWhV8WGuWfZ0g76BtKoAD3gmn5UCd1HhALuexXJw0eaaKsy83/6YJqcMh6NG\nZSVjKikefwKBgDuwQdCl5dAiSInqcJF9XelG8RfXnFVcMchMgXN42PWEH9036Hin\n/2qWGAYozDxLo5hl1SsSPZamUoxF9bEgy1GC83a954jNOxzHcKebX8D4/zDPeuz2\nWMwvkJdnCgearVc8/zZR3OoeAJ06d+xJG9avUiEF+PC+tbDfJSxMrySxAoGAYXA5\nVRNzKF1j+pIhZJbGuan9KZHTjjmxRkFdtjVp/W0Sz3oGZfvKKd3yzOkZN3Qib04k\njL/lKUJNRuZiZlkFIamU+CktluVtWPd6XXbbuw5Llsdc11qDOYBLMC24dl9s+CX7\n7mYaZOU/n/1OZOnaeDv9g0w37NOi6ux/TxNkO9cCgYBfGP65RdeJyFaFB5n2T44s\nmbvnws7YfgQ/IsGgKj/acUK/teCP9jN0H5bdl2x0JeqGdHsCMWdOxIkHyM4XAZv0\n/kGJ4syBlBWip9rjjT7Yu0blR0gpbf3eimIP0N1zfkOd2UYK+UQJVL1tUUg9JwfW\nV/2GwLDUhZx94yXOO3pCIw==\n-----END PRIVATE KEY-----\n",//process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+        projectId: "learningfirebase-marioplan", //process.env.PROJECT_ID,
+        clientEmail: "firebase-adminsdk-fqjdb@learningfirebase-marioplan.iam.gserviceaccount.com", // process.env.CLIENT_EMAIL,
     }),
 });
 
@@ -23,6 +23,6 @@ if (process.env.REDIS_URL) {
     console.log('Enabled Redis');
 }
 
-io.use(authenticate);
+//io.use(authenticate);
 io.on(Event.CONNECTION, connect);
-http.listen(process.env.PORT);
+http.listen(3000, '127.0.0.1');//process.env.PORT);
